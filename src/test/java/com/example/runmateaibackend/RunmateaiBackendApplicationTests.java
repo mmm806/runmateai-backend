48975c4e8c88741997497e9ce65ad142d2a1d08c
Kim EungJin <mmm806@naver.com>
2026-06-13 19:50:02 +0900

    🎉 feat: initial project setup

---
+++ src/test/java/com/example/runmateaibackend/RunmateaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.runmateaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunmateaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
